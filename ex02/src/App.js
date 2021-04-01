8fb724bfa717da434000d3d4d595aff82ba7069a
Niloufar Ghesmati <Niloofargst@gmail.com>
2021-04-01 02:24:14 +0200

    adding folders

---
+++ ex02/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  };
+
+  getMyStorage = () => {
+    const myCookieData = document.cookie.match(new RegExp("(^| )Year=([^;]+)"))[2];
+    const myLocalStorageDate = localStorage.getItem("Paragon");
+    const mySessionStorageData = sessionStorage.getItem("frontend");
+    console.log(myCookieData, myLocalStorageDate, mySessionStorageData);
+    return [myCookieData, myLocalStorageData, mySessionStorageData];
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setMyStorage}>Set Storage</button>
+        <button onClick={this.getMyStorage}>Get Storage</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ex04/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  setLocalStorage = () => {
+    localStorage.setItem("Arena", "Selection Month");
+  };
+
+  getLocalStorage = () => {
+    let myLocalStorageData = localStorage.getItem("Arena");
+    console.log(myLocalStorageData);
+    return myLocalStorageData;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setLocalStorage}>Set Local Storage</button>
+        <button onClick={this.getLocalStorage}>Get Local Storage</button>
+      </div>
+    );
+  }
+}
+
+export default App;
